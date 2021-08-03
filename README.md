bad926894a3050f3fcc47b7024941f544e0b1499
luismedinacoca <luismedinacoca@gmail.com>
2021-08-03 01:07:01 -0300

    lecture025

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-Lecture024 - Command activation & inspecting specific Cypress
+Lecture025 - Visit & Click commands
 
-1. adding the first command:
-```bash
-/// <reference types="Cypress" />
-```
-1. adding describe()
-1. adding it()
-1. adding first cypress code:
+1. adding more cypress code:
 ```javascript
-cy.click
+cy.visit("url");
+cy.get("locator").click();
+cy.click();
 ```
  
--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -1,14 +1,13 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
 
-describer("Test Contact Us form via WebdriverUni", () => {
+describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         //cypress code
-        cy.click()
-    });
-
+        cy.visit('https://webdriveruniversity.com/');
+        cy.get('#contact-us > .thumbnail > .section-title > h1').click()
+    }),
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
         //cypress code
-        
-    });
-    
+        cy.visit('https://webdriveruniversity.com/');
+    })
 })
