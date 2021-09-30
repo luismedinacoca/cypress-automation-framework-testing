b1eee018159ea579c70908ab99b4a703ec615244
luismedinacoca <luismedinacoca@gmail.com>
2021-09-30 08:41:12 -0300

    lecture076 variables part4

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -16,9 +16,28 @@ describe("Verifying cariables, cypress commands and jquery commands", () => {
         // makeupLink.click();
         // skincareLink.click();
 
-        //recommended approach:
+        //recommended cypress approach:
 
         cy.get("a[href*='category&path=']").contains("Skincare");
         cy.get("a[href*='category&path=']").contains("Makeup"); 
+    });
+
+    it.only("Navigating to specific product pages", () => {
+        //cypress code
+        cy.visit("https://automationteststore.com");
+        cy.get("a[href*='category&path=']").contains("Makeup").click(); 
+
+        //this code will fail => declaring variable or const
+        //const header = cy.get("h1 .maintext");
+        //cy.log(header);
+        //cy.log(header.text());
+
+        //using promises
+        cy.get("h1 .maintext").then(($headerText) => {
+            //variable or const declaration:
+            const headerText = $headerText.text();
+            cy.log("Found header text: " + headerText);
+            expect(headerText).is.eq('Makeup');
+        })
     });
 })
